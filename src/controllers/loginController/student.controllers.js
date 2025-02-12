cbc715a9d5e7f37061efaaeac760e4d6b3ebd30c
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-02-12 20:24:45 +0530

    login function created for student

--- src/controllers/loginController/student.controllers.js
+++ src/controllers/loginController/student.controllers.js
@@ -3,19 +3,21 @@ import { ApiError } from "../../utils/ApiError";
 import { ApiResponse } from "../../utils/ApiResponse";
 
 const studentLogin = asyncHandler(async (req, res) => {
-    const {id} = req.body;
+    const { id } = req.body;
 
-    if(!id) {
+    if (!id) {
         throw new ApiError(400, "Error filling the form");
     }
 
-    const student = await student.findOne({id});
+    const student = await student.findOne({ id });
 
-    if(!student) {
+    if (!student) {
         throw new ApiError(404, "Student id not found");
     }
 
-    return res.status(200).json(new ApiResponse(200, student, "Logged in successfully"));
-})
+    return res
+        .status(200)
+        .json(new ApiResponse(200, student, "Logged in successfully"));
+});
 
-export {studentLogin};
+export { studentLogin };
